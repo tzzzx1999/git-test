3c41d191d5633a02b6597e9c7a44ec31992f666c
tzx <tianzhengxuanttt@163.com>
2023-02-16 15:54:27 +0800

    first commit

---
+++ src/main/java/com/tzxlearn/git/gitTest.java
@@ -0,0 +1,12 @@
+package com.tzxlearn.git;
+
+/**
+ * @description:
+ * @author: 田正轩
+ * @time: 2023/2/16 15:51
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
