d6315fd10648bc1442a96e2a92a52b7b6d3514b0
tzx <tianzhengxuanttt@163.com>
2023-02-16 16:33:13 +0800

    second commit

--- src/main/java/com/tzxlearn/git/gitTest.java
+++ src/main/java/com/tzxlearn/git/gitTest.java
@@ -8,8 +8,8 @@ package com.tzxlearn.git;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git2!");
-        System.out.println("hello git2!");
-        System.out.println("hello git2!");
-        System.out.println("hello git2!");
+        System.out.println("hello git3!");
+        System.out.println("hello git3!");
+        System.out.println("hello git333333333333333333!");
     }
 }
